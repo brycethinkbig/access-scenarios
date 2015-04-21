8cad2024d584ace402a0107828f14a35cc0fb67d
Bryce Cottam <bryce.cottam@thinkbiganalytics.com>
2015-04-21 12:17:39 -0600

    deleted code I didn't like

--- tomcat-mapreduce-test-lib/src/main/java/com/thinkbig/tomcat/api/JobLauncherService.java
+++
@@ -1,8 +0,0 @@
-package com.thinkbig.tomcat.api;
-
-import java.io.IOException;
-
-public interface JobLauncherService<T>
-{
-	boolean launchJob(T jobConfiguration) throws IOException, InterruptedException;
-}
